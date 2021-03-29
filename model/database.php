d37d2f1b3d6594283a751cf6e25d5281a367b933
Jeffrey Haugh <jahaugh@mail.fhsu.edu>
2021-03-28 20:57:43 -0500

    Update host information

--- model/database.php
+++ model/database.php
@@ -1,8 +1,12 @@
 <?php 
 
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-    $password = 'sesame';
+    // $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
+    // $username = 'root';
+    // $password = 'sesame';
+
+    $dsn = 'mysql://i7w8gblnaqcptqip:mv9gufsfyku3gcep@g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ef1ia7lpwe16ynsg:host=g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=ef1ia7lpwe16ynsg';
+    $username = 'i7w8gblnaqcptqip';
+    $password = 'mv9gufsfyku3gcep';
 
     try {
         $db = new PDO($dsn, $username, $password);
